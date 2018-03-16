56ea6600bedab62c1bf1062a22db2acbb3847317
Darby Lim <thlim@robotis.com>
2018-03-17 01:47:08 +0900

    add function

--- lookso/servo.cpp
+++ lookso/servo.cpp
@@ -1,20 +1,53 @@
-#include "SERVO.h"
+#include "servo.h"
 
-Servo::Servo(){}
-Servo::~Servo(){}
-
-void Servo::attach(uint32_t pwm_pin, uint32_t freq)
+Servo::Servo()
 {
-  this->pwm_pin_ = pwm_pin;
+  pin_ = 3;
+  freq_ = 50;
+  res_ = 10;
 
-  drv_pwm_set_freq(pwm_pin_, freq);
-  drv_pwm_setup(pwm_pin_);
+  angle_range_.max = 180;
+  angle_range_.min = -180;
+
+  offset_.for_min_pulse = 0;
+  offset_.for_max_pulse = 0;
 }
 
-void Servo::write(uint32_t deg)
-{
-  uint32_t res = RESOLUTION;
+Servo::~Servo(){}
 
-  uint32_t duty = map(deg, 0, 180, 52, 102);
-  drv_pwm_set_duty(pwm_pin_, res, duty);
+void Servo::begin(uint16_t range, uint32_t freq, uint32_t res)
+{
+  angle_range_.max = range/2;
+  angle_range_.min = (-1) * (range/2);
+
+  freq_ = freq;
+  res_  = res;
+}
+
+void Servo::attach(uint8_t pin)
+{
+  pin_  = pin;
+
+  drv_pwm_set_freq(pin_, freq_);
+  drv_pwm_setup(pin_);
+}
+
+void Servo::offset(uint16_t min_pulse_offset, uint16_t max_pulse_offset)
+{
+  offset_.for_min_pulse = min_pulse_offset;
+  offset_.for_max_pulse = max_pulse_offset;
+}
+
+void Servo::write(uint32_t angle)
+{
+  uint32_t get_angle = angle;
+  uint16_t duty = 0;
+
+  uint32_t min_pulse = (uint32_t)(pow((double)(2), (double)(res_))*0.05) + offset_.for_min_pulse;
+  uint32_t max_pulse = (uint32_t)(pow((double)(2), (double)(res_))*0.1)  + offset_.for_max_pulse;
+
+  // angle = constrain(angle, angle_range_.min, angle_range_.max);    
+  duty = map(angle, angle_range_.min, angle_range_.max, min_pulse, max_pulse);
+
+  drv_pwm_set_duty(pin_, res_, duty);
 }
--- lookso/servo.h
+++ lookso/servo.h
@@ -1,15 +1,33 @@
 #include <Arduino.h>
 
-#define RESOLUTION  10
+typedef struct 
+{
+  int16_t max;
+  int16_t min;
+} angle_range_t;
+
+typedef struct
+{
+  int32_t for_min_pulse;
+  int32_t for_max_pulse;
+} offset_t;
 
 class Servo
 {
+ private:
+  uint8_t  pin_;
+  uint32_t freq_;
+  uint32_t res_;
+
+  angle_range_t angle_range_;
+  offset_t offset_;
+
  public:
   Servo();
   ~Servo();
 
-  uint32_t pwm_pin_;
-
-  void attach(uint32_t pwm_pin, uint32_t freq = 50);
-  void write(uint32_t duty);
+  void begin(uint16_t range = 120, uint32_t freq = 50, uint32_t res = 10);
+  void offset(uint16_t min_pulse_offset, uint16_t max_pulse_offset);
+  void attach(uint8_t pin);
+  void write(uint32_t angle);
 };
